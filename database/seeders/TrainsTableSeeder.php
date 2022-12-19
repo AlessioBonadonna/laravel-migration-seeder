621b4cfca77db06f60807c344e8562bf94c0b170
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-19 16:32:56 +0100

    add white faker in db

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
-use App\Models\TrainsModel;
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -17,18 +17,19 @@ class TrainsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i = 0; $i < 10; $i++) {
-            $new_train = new TrainsModel();
+            $new_train = new Train();
 
-            $new_train->azienda = $faker->words(5);
-            $new_train->st_Partenza = $faker->words(5);
-            $new_train->st_Arrivo = $faker->words(5);
+            $new_train->azienda = $faker->name();
+            $new_train->st_Partenza = $faker->city();
+            $new_train->st_Arrivo = $faker->city();
             $new_train->h_arrivo = $faker->time();
             $new_train->h_partenza = $faker->time();
-            $new_train->code_train = $faker->bothify(5);
+            $new_train->code_train = $faker->bothify();
             $new_train->n_carrozze = $faker->randomDigit();
             $new_train->orario = $faker->boolean();
             $new_train->cancellato = $faker->boolean();
-            //dd($new_train);
+
+            $new_train->save();
         }
     }
 }
