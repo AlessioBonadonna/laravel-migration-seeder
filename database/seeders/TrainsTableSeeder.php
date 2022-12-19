31f61aa8117c863d43703d455fe31f3902c43c96
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-19 16:04:40 +0100

    add faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\TrainsModel;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +14,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_train = new TrainsModel();
+
+            $new_train->azienda = $faker->words(5);
+            $new_train->st_Partenza = $faker->words(5);
+            $new_train->st_Arrivo = $faker->words(5);
+            $new_train->h_arrivo = $faker->time();
+            $new_train->h_partenza = $faker->time();
+            $new_train->code_train = $faker->bothify(5);
+            $new_train->n_carrozze = $faker->randomDigit();
+            $new_train->orario = $faker->boolean();
+            $new_train->cancellato = $faker->boolean();
+            //dd($new_train);
+        }
     }
 }
